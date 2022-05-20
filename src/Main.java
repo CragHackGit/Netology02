b2398d3831a7582e0a07b40e063f5a043aee1b04
Жернояров <craghackmul3@yandex.ru>
2022-05-20 17:19:48 +0300

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketCost = 10000;
+        int milesValue = 20;
+        int milesTotal = ticketCost / milesValue;
+        System.out.println("Количество бонусных Миль: " + milesTotal);
+    }
+}
